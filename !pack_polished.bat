91ba9e68368cdf72fc850a7f2a9aeb026657bd0b
Saint Wish <SaintWish@users.noreply.github.com>
2023-08-17 23:09:12 -0600

    output dir

--- !pack_polished.bat
+++ !pack_polished.bat
@@ -1,12 +1,8 @@
 @ECHO OFF
 
-set version_tag=ALP2017
+set version_tag=NOV2015b
+set out_path=_comp
 
-set betapack_dll_path=%msc_betapack_path%\dlls\
-set script_path=d:\hldev\scripts_msc\scripts\
-set script_path_m1=d:\hldev\scripts_msc\
-set out_path=f:\temp\scriptout\
-set steam_path=%msc_path%\dlls\
 :loop
 title Packing TEST SC.DLL %date% %time%
 echo REMEMBER TO UPDATE VERSION_TAG IN THIS BATCH FILE
@@ -19,44 +15,11 @@ rem if "%1"=="" goto :error_par
 cd .\scripts
 
 echo.
-echo ====================== Setting time stamp... 
-echo.
-
-echo.
-echo { >beta_date.script
-echo 	const BETA_TIMESTAMP "DEVELOPER %date% %time%" >>beta_date.script
-echo 	setvarg G_SCRIPT_VERSION %version_tag% >>beta_date.script
-echo } >>beta_date.script
-echo Done.
-
-rem echo.
-rem echo ====================== Clearing output folder... 
-rem echo.
-
-rem echo - Clear Files
-rem del /s /q %out_path%\*.*
-rem echo - Clear Folders
-rem rmdir /q /s  %out_path%
-rem echo - Remake path %out_path%
-rem md %out_path%
-rem echo Done.
-rem echo.
-rem echo If there were errors, check that %out_path% exists and is empty.
-rem pause
-
-echo.
-echo ====================== Cooking Scripts... 
-echo.
-
-..\cookscripts %script_path% %out_path%
-echo Done.
-
-echo.
 echo ====================== Porting Item List and Script Packer...  
 echo.
 
 rem copy %script_path%\items.txt %out_path%
-copy %script_path%\..\ScriptPack.exe %out_path%
+copy .\ScriptPack.exe %out_path%
 echo Done.
 
 echo.
@@ -65,23 +28,6 @@ echo.
 
 cd /d %out_path%
 ScriptPack.exe
-echo Done.
-
-echo.
-echo ====================== Propigating...
-echo.
-
-echo To scripts folder:
-cd /d %script_path%..
-copy %out_path%sc.dll %script_path%..
-move sc.dll %script_path%..
-echo To local client:
-copy sc.dll %steam_path%
-echo To beta pack:
-copy sc.dll %betapack_dll_path%
-rem echo To laptop HLDS:
-rem copy sc.dll" "\\Miniagnus3\hlds\msc\dlls\"
-cd..
 echo Done.
 
 echo.
